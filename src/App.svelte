55d1cd284cbb19754f96d4845424a9f4ced0bc9a
kanjo <kanjo.school@gmail.com>
2021-03-13 03:51:36 -0400

    Ajour props author

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,17 @@
 <script>
-import { text } from "svelte/internal";
-
+    import { text } from "svelte/internal";
+    import Message from "./Message.svelte";
     // Nom de l'application
 	export let name;
-    // Message a envoyer
-    let message = "";
     // Liste des messages
     let messages = [];
-
-    // Attribue la valeur du texte area à la variable message
-    // const updateMessage = (event) => {
-    //     console.log(event.target.value);
-    //     message = event.target.value;
-    // }
-    
-    // Ajoute un message à la liste des messages
-    const saveMessage = () => {
-        // Un message
-        const newMessage = {
-            id: Date.now(),
-            text: message,
-            author: "Jonathan"
-        };
-        // Ajout du message au début de la liste des messages
-        messages = [newMessage, ...messages];
-        //console.log('messages', messages);
-        // Reset de la variable message
-        message = "";
-    }
 </script>
 
 <main>
 	<h1>{name}!</h1>
-    <textarea cols="50" rows="5" bind:value={message}></textarea>
-    <br>
-    <button on:click={saveMessage}>Send</button>
-    <div>
-        <h3>Preview</h3>
-        {message}
-    </div>
+    
+    <Message author="Jojo"/>
+
     <div>
         <h2>Messages</h2>
         <!-- Parcourir la liste des messages -->
---
+++ src/Message.svelte
@@ -0,0 +1,28 @@
+<script>
+    // Message a envoyer
+    let message = "";
+    // Props author
+    export let author;
+    // Ajoute un message à la liste des messages
+    const saveMessage = () => {
+        // Structure d'un message
+        const newMessage = {
+            id: Date.now(),
+            text: message,
+            author: author,
+        };
+        // Ajout du message au début de la liste des messages
+        //messages = [newMessage, ...messages];
+        console.log('newMessage', newMessage);
+        // Reset de la variable message
+        message = "";
+    }
+</script>
+
+<main>
+    <textarea cols="50" rows="5" bind:value={message}></textarea>
+    <br>
+    <button on:click={saveMessage}>Send</button>
+</main>
+
+<style></style>
