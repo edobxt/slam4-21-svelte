2df38efe8e5cd3501eaeb5fd021a84adc114c654
kanjo <kanjo.school@gmail.com>
2021-03-13 21:42:34 -0400

    Ajout message d'erreur

--- src/Message.svelte
+++ src/Message.svelte
@@ -9,10 +9,12 @@
     let message = "";
     // Auteur du message
     let author = "";
+    // Nombre maximal de caractère d'un message
+    let maxLength = 24;
     // Longueur du message en temps réel
     $: nbCaracters = message.length;
     // Désactiver le bouton après un certain nombre de caractères
-    $: disabled = message.length > 12 ? true : false;
+    $: disabled = message.length > maxLength ? true : false;
 
     // Ajoute un message à la liste des messages
     const saveMessage = () => {
@@ -20,7 +22,7 @@
         const newMessage = {
             id: Date.now(),
             text: message,
-            author: author,
+            author: author || 'anonymous',
             date: new Date(),
         };
         // Ajout du message au début de la liste des messages
@@ -41,11 +43,20 @@
     <textarea cols="50" rows="5" bind:value={message}></textarea>
     <br>
     <!-- Bouton désactiver si disabled est vaut true -->
-    <button on:click={saveMessage} disabled={disabled}>Send</button> <span>{nbCaracters}</span>
+    <button on:click={saveMessage} disabled={disabled}>Send</button> 
+    <!-- Changement de couleurs lors du dépassement du nbre limite de caractère -->
+    <span class:alert={nbCaracters > maxLength}>{nbCaracters}</span>
+    <!-- Message d'erreur lorsque le bouton est désactivé -->
+    {#if disabled}
+        <span class="alert"> (message too long)</span>
+    {/if}
 </main>
 
 <style>
     .text {
         width: 382px;
+    }
+    .alert {
+        color: orangered;
     }
 </style>
